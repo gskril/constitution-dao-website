bd1956daf957d2083b95b9eb645d033d71a6f5a2
Greg <gskril@gmail.com>
2021-11-17 15:33:59 -0500

    Fix countdown timer

--- js/script.js
+++ js/script.js
@@ -34,7 +34,7 @@ setInterval(function(){
 	})()
 }, 10000);
 
-var countDownDate = new Date("Nov 18, 2021 15:00:00").getTime();
+var countDownDate = new Date("Nov 18, 2021 18:00:00").getTime();
 
 setInterval(function() {
 	var now = new Date().getTime();
@@ -44,17 +44,19 @@ setInterval(function() {
 
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var daysLabel = days !== 1 ? " days, " : " day, ";
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var hoursLabel = hours !== 1 ? " hours, " : " hour, ";
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var minutesLabel = minutes !== 1 ? " minutes " : " minute ";
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="demo"
-  document.getElementById("countdown").innerHTML = days + " days " + hours + " hours "
-  + minutes + " minutes" + " until auction";
 
   // If the count down is finished, write some text
   if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "Auction Today!";
+	  document.getElementById("countdown").innerHTML = "It's auction time!!";
+  } else {
+	  // Display the result in the element with id="demo"
+	  document.getElementById("countdown").innerHTML = days + daysLabel + hours + hoursLabel
+	  + minutes + minutesLabel + " until auction";
   }
 }, 1000)
